9a07d7baf9b059ad157773b84d7fc4d2415732ea
tomoya <shima.apple0941@gmail.com>
2023-03-22 17:27:44 +0900

    made web site responsive

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@
    Swiper.js
    ========================================================================== */
 const swiper = new Swiper(".swiper", {
-  slidesPerView: "auto",  
+  slidesPerView: "auto",
   grabCursor: true,
   loop: true,
 });
 
 /* ==========================================================================
-   
+   header navigation
    ========================================================================== */
+const header = document.querySelector(".l-header");
+const headerButton = document.querySelector(".l-header__button");
+const headerWrapper = document.querySelector(".l-header__wrapper");
+
+headerButton.addEventListener("click", () => {
+  header.classList.toggle("active");
+  headerButton.classList.toggle("active");
+  headerWrapper.classList.toggle("active");
+});
